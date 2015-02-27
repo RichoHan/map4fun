aede3a1da4d56fd26e234072ae6ee4b6c5c5a91a
Richo <richo2192000@gmail.com>
2015-02-27 17:05:15 +0800

    Visualization test v1

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask
+import json
+from pprint import pprint
+
+from flask import Flask, jsonify
 from flask import render_template
 app = Flask(__name__)
 
@@ -6,6 +9,25 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/data')
+def readData():
+    json_data = open('static/data/data.json')
+    data = json.load(json_data)
+    pprint(data)
+    json_data.close()
+    return jsonify(**data)
+
+
+@app.route('/geodata')
+def readGeoData():
+    json_data = open('static/data/points.geojson')
+    data = json.load(json_data)
+    pprint(data)
+    json_data.close()
+    return jsonify(**data)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
