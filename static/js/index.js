f37d1b486b4bbb2cdcbbb686fea6aed88d2ffa6d
Richo <richo2192000@gmail.com>
2015-03-29 20:21:33 +0800

    Add control panel

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,10 @@
 var width = 800,
     height = 750;
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".container").append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .attr("class", "map");
 
 var projection = d3.geo.mercator()
     .center([121.1722058, 24.6869505])
@@ -27,23 +28,37 @@ d3.json("hsinchu", function(error, hsinchu) {
 d3.json("data", function(error, data) {
     if (error) return console.error(error);
     
-    var list = data.features.filter(function(d) {
-        // "民宿之旅"
-        // "原民部落"
-        // "古蹟文化"
+    var list_a = data.features.filter(function(d) {
+        return d.properties.類別=="民宿之旅";
+    });
+
+    var list_b = data.features.filter(function(d) {
+        return d.properties.類別=="原民部落";
+    });
+
+    var list_c = data.features.filter(function(d) {
         return d.properties.類別=="古蹟文化";
     });
-    console.log(list);
     
     // points
-    var array = new Array();
-    for (var i=0 ; i<list.length ; i++){
-        array.push(list[i].geometry.coordinates);
+    var array_a = new Array();
+    for (var i=0 ; i<list_a.length ; i++){
+        array_a.push(list_a[i].geometry.coordinates);
+    }
+
+    var array_b = new Array();
+    for (var i=0 ; i<list_b.length ; i++){
+        array_b.push(list_b[i].geometry.coordinates);
+    }
+
+    var array_c = new Array();
+    for (var i=0 ; i<list_c.length ; i++){
+        array_c.push(list_c[i].geometry.coordinates);
     }
 
     // add circles to svg
-    svg.selectAll("circle")
-        .data(array).enter()
+    svg.selectAll(".map")
+        .data(array_a).enter()
         .append("circle")
         .attr("cx", function (d) {
             return projection(d)[0];
@@ -52,6 +67,63 @@ d3.json("data", function(error, data) {
             return projection(d)[1];
         })
         .attr("r", "2px")
-        .attr("fill", "#ECF5FF")
+        .attr("class", "a")
+        .attr("display", "none");
+
+    svg.selectAll(".map")
+        .data(array_b).enter()
+        .append("circle")
+        .attr("cx", function (d) {
+            return projection(d)[0];
+        })
+        .attr("cy", function (d) {
+            return projection(d)[1];
+        })
+        .attr("r", "2px")
+        .attr("class", "b")
+        .attr("display", "none");
+
+    svg.selectAll(".map")
+        .data(array_c).enter()
+        .append("circle")
+        .attr("cx", function (d) {
+            return projection(d)[0];
+        })
+        .attr("cy", function (d) {
+            return projection(d)[1];
+        })
+        .attr("r", "2px")
+        .attr("class", "c")
+        .attr("display", "none");
 
 });
+
+$('.ui.checkbox.place_a')
+    .checkbox({
+        onUnchecked: function() {
+            $('.a').hide();
+        },onChecked: function() {
+            $('.a').show();
+        }
+    })
+;
+
+$('.ui.checkbox.place_b')
+    .checkbox({
+        onUnchecked: function() {
+            $('.b').hide();
+        },onChecked: function() {
+            $('.b').show();
+        }
+    })
+;
+
+$('.ui.checkbox.place_c')
+    .checkbox({
+        onUnchecked: function() {
+            $('.c').hide();
+        },onChecked: function() {
+            $('.c').show();
+        }
+    })
+;
