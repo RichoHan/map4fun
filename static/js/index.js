990bf50eeff8bc3da97ca3db3efd83fd1d972c89
Richo <richo2192000@gmail.com>
2015-03-29 15:33:16 +0800

    Delete example json file

--- static/js/index.js
+++ static/js/index.js
@@ -1,28 +1,3 @@
-// var map = new Datamap({
-//     element: document.getElementById('container'),
-    
-//     geographyConfig: {
-//         dataUrl: '/hsinchu'
-//     },
-
-//     scope: 'custom',
-
-//     setProjection: function(element, options) {
-//         var projection, path;
-
-//         projection = d3.geo.albersUsa()
-//         .scale(element.offsetWidth)
-//         .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-    
-//         path = d3.geo.path()
-//         .projection( projection );
-
-//         return {
-//             path: path, projection: projection
-//         };
-//     }
-// });
-
 var width = 800,
     height = 750;
 
