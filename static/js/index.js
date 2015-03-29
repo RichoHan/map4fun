1c71b0aa3f820dc38e404801499362b2d0fc6df8
Richo <richo2192000@gmail.com>
2015-03-29 15:26:33 +0800

    First Hsinchu map based on topojson

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,49 @@
-<script>
-    var map = new Datamap({
-        element: document.getElementById('container'),
-        geographyConfig: {
-            dataUrl: '/hsinchu'
-        },
-        scope: 'custom',
-        setProjection: function(element, options) {
-            var projection, path;
-            projection = d3.geo.albersUsa()
-                .scale(element.offsetWidth)
-                .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
-}
-            path = d3.geo.path()
-                .projection( projection );
+// var map = new Datamap({
+//     element: document.getElementById('container'),
+    
+//     geographyConfig: {
+//         dataUrl: '/hsinchu'
+//     },
 
-            return {path: path, projection: projection};
-        }
-    });
-</script>
+//     scope: 'custom',
+
+//     setProjection: function(element, options) {
+//         var projection, path;
+
+//         projection = d3.geo.albersUsa()
+//         .scale(element.offsetWidth)
+//         .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+    
+//         path = d3.geo.path()
+//         .projection( projection );
+
+//         return {
+//             path: path, projection: projection
+//         };
+//     }
+// });
+
+var width = 800,
+    height = 750;
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.json("hsinchu", function(error, hsinchu) {
+    if (error) return console.error(error);
+    
+    var hsinchu = topojson.feature(hsinchu, hsinchu.objects.Hsinchu)
+    
+    var projection = d3.geo.mercator()
+        .center([120.9564763, 24.7847717])
+        .scale(250000)
+        .translate([width / 2, height / 2]);
+    
+    var path = d3.geo.path()
+        .projection(projection);
+
+    svg.append("path")
+        .datum(hsinchu)
+        .attr("d", path);
+});
